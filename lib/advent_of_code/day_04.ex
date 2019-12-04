8c25f89867f2bc66d41ec82ba8026831c247876e
Giacomo Magnanini <gikappa@gmail.com>
2019-12-04 13:04:15 +0100

    day 4 refactor & optimize

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,35 +1,31 @@
 defmodule AdventOfCode.Day04 do
   def part1(range) do
-    for x <- range do
-      Task.async(fn -> valid_password?(x) end)
-    end
-    |> Enum.map(&Task.await/1)
+    range
+    |> Enum.map(&valid_password?/1)
     |> Enum.filter(& &1)
     |> Enum.count()
   end
 
   def part2(range) do
-    for x <- range do
-      Task.async(fn -> valid_password_2?(x) end)
-    end
-    |> Enum.map(&Task.await/1)
+    range
+    |> Enum.map(&valid_password_2?/1)
     |> Enum.filter(& &1)
     |> Enum.count()
   end
 
   def valid_password?(password) do
-    adjacent_digits?(password) && increasing?(password)
+    increasing?(password) && adjacent_digits?(password)
   end
 
   def valid_password_2?(password) do
-    adjacent_digits_2?(password) && increasing?(password)
+    increasing?(password) && adjacent_digits_2?(password)
   end
 
-  def adjacent_digits?(password) do
+  defp adjacent_digits?(password) do
     password |> Integer.to_string() |> String.match?(~r/(.)\1+/)
   end
 
-  def adjacent_digits_2?(password) do
+  defp adjacent_digits_2?(password) do
     password
     |> Integer.digits()
     |> Enum.reduce(%{}, fn x, acc ->
@@ -38,17 +34,8 @@ defmodule AdventOfCode.Day04 do
     |> Enum.any?(fn {_, v} -> v == 2 end)
   end
 
-  def increasing?(password) do
-    {_, inc} =
-      password
-      |> Integer.digits()
-      |> Enum.reduce_while({0, true}, fn x, {last, _} ->
-        case x do
-          x when x >= last -> {:cont, {x, true}}
-          _ -> {:halt, {nil, false}}
-        end
-      end)
-
-    inc
+  defp increasing?(password) do
+    [a, b, c, d, e, f] = Integer.digits(password)
+    f >= e && e >= d && d >= c && c >= b && b >= a
   end
 end
