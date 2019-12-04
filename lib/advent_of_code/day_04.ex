dcffb6a6152b66a1d37c44a3811140fa575a4da0
Giacomo Magnanini <gikappa@gmail.com>
2019-12-04 12:23:08 +0100

    day 4

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,54 @@
 defmodule AdventOfCode.Day04 do
-  def part1(args) do
+  def part1(range) do
+    for x <- range do
+      Task.async(fn -> valid_password?(x) end)
+    end
+    |> Enum.map(&Task.await/1)
+    |> Enum.filter(& &1)
+    |> Enum.count()
   end
 
-  def part2(args) do
+  def part2(range) do
+    for x <- range do
+      Task.async(fn -> valid_password_2?(x) end)
+    end
+    |> Enum.map(&Task.await/1)
+    |> Enum.filter(& &1)
+    |> Enum.count()
+  end
+
+  def valid_password?(password) do
+    adjacent_digits?(password) && increasing?(password)
+  end
+
+  def valid_password_2?(password) do
+    adjacent_digits_2?(password) && increasing?(password)
+  end
+
+  def adjacent_digits?(password) do
+    password |> Integer.to_string() |> String.match?(~r/(.)\1+/)
+  end
+
+  def adjacent_digits_2?(password) do
+    password
+    |> Integer.digits()
+    |> Enum.reduce(%{}, fn x, acc ->
+      Map.update(acc, x, 1, &(&1 + 1))
+    end)
+    |> Enum.any?(fn {_, v} -> v == 2 end)
+  end
+
+  def increasing?(password) do
+    {_, inc} =
+      password
+      |> Integer.digits()
+      |> Enum.reduce_while({0, true}, fn x, {last, _} ->
+        case x do
+          x when x >= last -> {:cont, {x, true}}
+          _ -> {:halt, {nil, false}}
+        end
+      end)
+
+    inc
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,19 +3,20 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
+  # @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+    assert valid_password?(111_111)
+    assert valid_password?(122_345)
+    assert valid_password?(111_123)
+    assert not valid_password?(223_450)
+    assert not valid_password?(123_789)
+    assert not valid_password?(445_179)
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
+    assert valid_password_2?(112_233)
+    assert not valid_password_2?(123_444)
+    assert valid_password_2?(111_122)
   end
 end
