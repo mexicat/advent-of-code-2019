65936ddb1f3ef7175059eff64f572d332cc3f2d7
Giacomo Magnanini <gikappa@gmail.com>
2019-12-15 19:30:03 +0100

    day 15

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,7 +1,70 @@
 defmodule AdventOfCode.Day15 do
-  def part1(args) do
+  def part1(program) do
+    initial = IntCodeFun.new(program, nil)
+
+    search(initial, {0, 0}, 0, %{visited: %{}, walls: MapSet.new(), oxygen: nil})
   end
 
-  def part2(args) do
+  def part2(program) do
+    state = program |> part1()
+    visited = state.visited |> Map.new(fn {k, _} -> {k, " "} end)
+    walls = state.walls |> Map.new(fn pos -> {pos, "#"} end)
+
+    grid =
+      walls
+      |> Map.merge(visited)
+      |> Map.put(state.oxygen, "O")
+
+    {_, steps} = fill(grid, state.oxygen, 0, 0)
+    steps
   end
+
+  def fill(grid, pos, steps, max_steps) do
+    Enum.reduce(1..4, {grid, max_steps}, fn dir, {grid, max_steps} ->
+      with new_pos <- move(pos, dir),
+           true <- Map.get(grid, new_pos) == " " do
+        fill(Map.put(grid, new_pos, "O"), new_pos, steps + 1, max(max_steps, steps + 1))
+      else
+        false ->
+          {grid, max(max_steps, steps)}
+      end
+    end)
+  end
+
+  def search(intcode, pos, steps, state = %{visited: visited, walls: walls}) do
+    Enum.reduce(1..4, state, fn dir, state ->
+      with new_pos <- move(pos, dir),
+           true <- not Enum.member?(walls, new_pos),
+           true <- not Map.has_key?(visited, new_pos) do
+        {:out, intcode} =
+          intcode
+          |> IntCodeFun.set_input(dir)
+          |> IntCodeFun.run_until_output()
+
+        case intcode.input do
+          0 ->
+            %{state | walls: MapSet.put(state.walls, new_pos)}
+
+          # add to visited and recursively search there
+          1 ->
+            search(intcode, new_pos, steps + 1, %{
+              state
+              | visited: Map.put(state.visited, new_pos, intcode)
+            })
+
+          # found oxygen
+          2 ->
+            IO.puts(steps + 1)
+            %{state | oxygen: new_pos}
+        end
+      else
+        false -> state
+      end
+    end)
+  end
+
+  def move({x, y}, 1), do: {x, y + 1}
+  def move({x, y}, 2), do: {x, y - 1}
+  def move({x, y}, 3), do: {x - 1, y}
+  def move({x, y}, 4), do: {x + 1, y}
 end
---
+++ lib/advent_of_code/intcode_fun.ex
@@ -0,0 +1,194 @@
+defmodule IntCodeFun do
+  @enforce_keys [:phase, :input, :opcodes]
+  defstruct [:phase, :input, :opcodes, at: 0, rel_base: 0]
+
+  def new(opcodes, phase) do
+    %IntCode{phase: phase, input: 0, opcodes: opcodes}
+  end
+
+  def set_input(intcode, input) do
+    Map.put(intcode, :input, input)
+  end
+
+  def run(intcode) do
+    Stream.iterate({:cont, intcode}, fn
+      {:out, intcode} ->
+        next(intcode)
+
+      {:cont, intcode} ->
+        next(intcode)
+
+      {:stop, intcode} ->
+        IO.inspect(intcode)
+        :stop
+    end)
+    |> Enum.find(&(&1 == :stop))
+  end
+
+  def run_until_output(intcode) do
+    Stream.iterate({:cont, intcode}, fn
+      {:out, intcode} ->
+        {:out, intcode}
+
+      {:cont, intcode} ->
+        next(intcode)
+
+      {:stop, intcode} ->
+        IO.inspect(intcode)
+        :stop
+    end)
+    |> Enum.find(&(is_tuple(&1) && elem(&1, 0) == :out))
+  end
+
+  def next(intcode = %{opcodes: opcodes, at: at}) do
+    to_parse = Enum.drop(opcodes, at)
+    [op | args] = to_parse
+    {modes, operation} = identify_op(op)
+
+    case operation do
+      1 ->
+        add(
+          intcode,
+          target(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode),
+          target(Enum.at(args, 1), Enum.at(modes, 1, 0), intcode),
+          at(Enum.at(args, 2), Enum.at(modes, 2, 0), intcode)
+        )
+
+      2 ->
+        multiply(
+          intcode,
+          target(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode),
+          target(Enum.at(args, 1), Enum.at(modes, 1, 0), intcode),
+          at(Enum.at(args, 2), Enum.at(modes, 2, 0), intcode)
+        )
+
+      3 ->
+        save(intcode, at(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode))
+
+      4 ->
+        output(intcode, target(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode))
+
+      5 ->
+        jump_if_true(
+          intcode,
+          target(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode),
+          target(Enum.at(args, 1), Enum.at(modes, 1, 0), intcode)
+        )
+
+      6 ->
+        jump_if_false(
+          intcode,
+          target(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode),
+          target(Enum.at(args, 1), Enum.at(modes, 1, 0), intcode)
+        )
+
+      7 ->
+        less_than(
+          intcode,
+          target(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode),
+          target(Enum.at(args, 1), Enum.at(modes, 1, 0), intcode),
+          at(Enum.at(args, 2), Enum.at(modes, 2, 0), intcode)
+        )
+
+      8 ->
+        equals(
+          intcode,
+          target(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode),
+          target(Enum.at(args, 1), Enum.at(modes, 1, 0), intcode),
+          at(Enum.at(args, 2), Enum.at(modes, 2, 0), intcode)
+        )
+
+      9 ->
+        rel_base_offset(intcode, target(Enum.at(args, 0), Enum.at(modes, 0, 0), intcode))
+
+      99 ->
+        terminate(intcode)
+    end
+  end
+
+  def identify_op(op) do
+    operation = rem(op, 100)
+    modes = op |> div(100) |> Integer.digits() |> Enum.reverse()
+    {modes, operation}
+  end
+
+  def add(intcode, a, b, c) do
+    opcodes = replace_at(intcode.opcodes, c, a + b)
+    {:cont, %{intcode | opcodes: opcodes, at: intcode.at + 4}}
+  end
+
+  def multiply(intcode, a, b, c) do
+    opcodes = replace_at(intcode.opcodes, c, a * b)
+    {:cont, %{intcode | opcodes: opcodes, at: intcode.at + 4}}
+  end
+
+  def save(intcode, a) do
+    opcodes =
+      replace_at(
+        intcode.opcodes,
+        a,
+        if(intcode.phase, do: intcode.phase, else: intcode.input)
+      )
+
+    {:cont, %{intcode | opcodes: opcodes, phase: nil, at: intcode.at + 2}}
+  end
+
+  def output(intcode, a) do
+    # IO.puts("output: #{a}")
+    {:out, %{intcode | input: a, at: intcode.at + 2}}
+  end
+
+  def jump_if_true(intcode, a, b) do
+    at = if a != 0, do: b, else: intcode.at + 3
+    {:cont, %{intcode | at: at}}
+  end
+
+  def jump_if_false(intcode, a, b) do
+    at = if a == 0, do: b, else: intcode.at + 3
+    {:cont, %{intcode | at: at}}
+  end
+
+  def less_than(intcode, a, b, c) do
+    opcodes = replace_at(intcode.opcodes, c, if(a < b, do: 1, else: 0))
+    {:cont, %{intcode | opcodes: opcodes, at: intcode.at + 4}}
+  end
+
+  def equals(intcode, a, b, c) do
+    opcodes = replace_at(intcode.opcodes, c, if(a == b, do: 1, else: 0))
+    {:cont, %{intcode | opcodes: opcodes, at: intcode.at + 4}}
+  end
+
+  def rel_base_offset(intcode, a) do
+    {:cont, %{intcode | rel_base: intcode.rel_base + a, at: intcode.at + 2}}
+  end
+
+  def terminate(intcode) do
+    {:stop, intcode}
+  end
+
+  def target(arg, mode, intcode) do
+    case mode do
+      1 -> arg
+      2 -> Enum.at(intcode.opcodes, arg + intcode.rel_base, 0)
+      0 -> Enum.at(intcode.opcodes, arg, 0)
+    end
+  end
+
+  def at(pos, mode, intcode) do
+    case mode do
+      0 -> pos
+      2 -> pos + intcode.rel_base
+      x -> raise ArgumentError, "invalid position #{x}"
+    end
+  end
+
+  def replace_at(list, index, value) do
+    list =
+      cond do
+        length(list) < index + 1 -> list ++ List.duplicate(0, index - length(list) + 1)
+        true -> list
+      end
+
+    List.replace_at(list, index, value)
+  end
+end
