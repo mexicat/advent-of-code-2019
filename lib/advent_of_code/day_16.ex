e444d375f0a010244be12bd8a4f0593519aa2c03
Giacomo Magnanini <gikappa@gmail.com>
2019-12-16 16:09:30 +0100

    day 16

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -1,7 +1,67 @@
 defmodule AdventOfCode.Day16 do
-  def part1(args) do
+  def part1(input, pattern \\ [0, 1, 0, -1]) do
+    input
+    |> Integer.digits()
+    |> do_fft(pattern, 100)
+    |> Enum.take(8)
+    |> Enum.join()
   end
 
-  def part2(args) do
+  def part2(input) do
+    digits =
+      input
+      |> Integer.digits()
+
+    offset = digits |> Enum.take(7) |> Enum.join() |> String.to_integer()
+
+    digits
+    |> List.duplicate(10000)
+    |> List.flatten()
+    |> Enum.drop(offset)
+    |> do_fft_2(100)
+    |> Enum.take(8)
+    |> Enum.join()
+  end
+
+  def do_fft(signal, pattern, phases) do
+    Enum.reduce(1..phases, signal, fn ph, acc ->
+      IO.puts(ph)
+      apply_phase(acc, pattern)
+    end)
+  end
+
+  def do_fft_2(signal, phases) do
+    Enum.reduce(1..phases, signal, fn ph, signal ->
+      IO.puts(ph)
+
+      signal
+      |> Enum.reverse()
+      |> Enum.scan(0, fn n, acc -> (n + acc) |> abs() |> rem(10) end)
+      |> Enum.reverse()
+    end)
+  end
+
+  def apply_phase(signal, pattern) do
+    Enum.map(Enum.with_index(signal), fn {_n, pos} ->
+      pattern =
+        pattern
+        |> pattern_for_pos(pos)
+        |> Stream.cycle()
+        |> Stream.drop(1)
+
+      signal
+      |> Enum.zip(pattern)
+      |> Enum.reduce(0, fn {n, operand}, acc ->
+        n * operand + acc
+      end)
+      |> abs()
+      |> rem(10)
+    end)
+  end
+
+  def pattern_for_pos(pattern, 0), do: pattern
+
+  def pattern_for_pos(pattern, pos) do
+    for(p <- pattern, do: List.duplicate(p, pos + 1)) |> List.flatten()
   end
 end
--- test/advent_of_code/day_16_test.exs
+++ test/advent_of_code/day_16_test.exs
@@ -3,12 +3,24 @@ defmodule AdventOfCode.Day16Test do
 
   import AdventOfCode.Day16
 
-  @tag :skip
+  # @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    pattern = [0, 1, 0, -1]
 
-    assert result
+    assert String.starts_with?(
+             part1(80_871_224_585_914_546_619_083_218_645_595),
+             "24176176"
+           )
+
+    assert String.starts_with?(
+             part1(19_617_804_207_202_209_144_916_044_189_917),
+             "73745418"
+           )
+
+    assert String.starts_with?(
+             part1(69_317_163_492_948_606_335_995_924_319_873),
+             "52432133"
+           )
   end
 
   @tag :skip
